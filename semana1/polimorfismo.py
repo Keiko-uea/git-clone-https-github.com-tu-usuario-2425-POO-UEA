0a7ae9f97f3ceee50a181a3088874d6372e5bd8f
keiko <kd.yanacalloo@uea.edu.ec>
2025-01-31 11:24:43 -0500

    ultima version

--- semana1/polimorfismo.py
+++ semana1/polimorfismo.py
@@ -1,19 +1,26 @@
-class Vehiculo:
-    def mover(self):
-        raise NotImplementedError("Este método debe ser implementado por subclases")
-class Coche(Vehiculo):
-    def mover(self):
-        return "El coche se mueve por la carretera."
 
-class Bicicleta(Vehiculo):
-    def mover(self):
-        return "La bicicleta se mueve pedaleando."
-def main():
-    vehiculos = [Coche(), Bicicleta()]
+import threading
+import time
 
-    for vehiculo in vehiculos:
-        print(vehiculo.mover())
+# Función que simula una tarea para un hilo
+def tarea_hilo(identificador, delay):
+    for i in range(5):
+        print(f'Hilo {identificador}: Realizando tarea {i}')
+        time.sleep(delay)
 
-if __name__ == "__main__":
-    main()
+# Crear instancias de hilos
+hilo1 = threading.Thread(target=tarea_hilo, args=(1, 1))
+hilo2 = threading.Thread(target=tarea_hilo, args=(2, 0.8))
+hilo3 = threading.Thread(target=tarea_hilo, args=(3, 1.2))
 
+# Iniciar los hilos
+hilo1.start()
+hilo2.start()
+hilo3.start()
+
+# Esperar a que todos los hilos terminen
+hilo1.join()
+hilo2.join()
+hilo3.join()
+
+print('Programa principal: Todas las tareas han sido completadas.')
